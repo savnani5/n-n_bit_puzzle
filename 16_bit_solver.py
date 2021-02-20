9ad04c294254ec6cbd8ce2ded982c86bbd6583f5
“[Paras <[savnani5@gmail.com]>
2021-02-20 17:25:43 -0500

    Class based implementation of the puzzle solver

--- 16_bit_solver.py
+++ 16_bit_solver.py
@@ -1,68 +1,78 @@
-import copy
 import time
+import copy
 from collections import deque
 import numpy as np
 
-# string is fast in comparion but searching and conversion from list to string will be time consuming
+# String is fast in comparion but searching and conversion from list to string will be time consuming
 # Cannot implement binay search because we have to search in unsorted array
 
+class Node: 
 
-def up(pos, state):
-    x,y = pos[0], pos[1]
-    n = len(state) - 1
-    if x != 0:
-        state[x][y], state[x-1][y] = state[x-1][y], state[x][y]
-        return state
-    return None
+    def __init__(self, n, state, parent):
+        self.n = n                              # n is the 1D dimension of the puzzle for eg: for a 4*4 puzzle, n will be 4
+        self.state = state                      # state is the current state of the puzzle
+        self.parent = parent                    # parent attribute stores the parent node of current node
 
-def down(pos, state):
-    x,y = pos[0], pos[1]
-    n = len(state) - 1
-    if x != n:
-        state[x][y], state[x+1][y] = state[x+1][y], state[x][y]
-        return state
-    return None
+    def __repr__(self):
+        return str(self.state)                  # This method returns the state information of the node
 
-def left(pos, state):
-    x,y = pos[0], pos[1]
-    n = len(state) - 1
-    if y != 0:
-        state[x][y], state[x][y-1] = state[x][y-1], state[x][y]
-        return state
-    return None
+    def up(self, pos):
+        x,y = pos[0], pos[1]
+        if x != 0:
+            up_node = Node(self.n, copy.deepcopy(self.state), Node(self.n, self.state, self.parent))
+            up_node.state[x][y], up_node.state[x-1][y] = up_node.state[x-1][y], up_node.state[x][y]
+            return up_node
+        return None
 
-def right(pos, state):
-    x,y = pos[0], pos[1]
-    n = len(state) - 1
-    if y != n:
-        state[x][y], state[x][y+1] = state[x][y+1], state[x][y]
-        return state
-    return None
+    def down(self, pos):
+        x,y = pos[0], pos[1]
+        if x != (self.n - 1):
+            down_node = Node(self.n, copy.deepcopy(self.state), Node(self.n, self.state, self.parent))
+            down_node.state[x][y], down_node.state[x+1][y] = down_node.state[x+1][y], down_node.state[x][y]
+            return down_node
+        return None
 
-def generate_children(pos, current_state):
-    
-    children = []
-    up_move = up(pos, copy.deepcopy(current_state))
-    down_move = down(pos, copy.deepcopy(current_state))
-    left_move = left(pos, copy.deepcopy(current_state))
-    right_move = right(pos, copy.deepcopy(current_state))
-    
-    if up_move:
-        children.append(up_move) 
-    if down_move:
-        children.append(down_move)
-    if left_move:
-        children.append(left_move)
-    if right_move:
-        children.append(right_move)
-    
-    return children
+    def left(self, pos):
+        x,y = pos[0], pos[1]
+        if y != 0:
+            left_node = Node(self.n, copy.deepcopy(self.state), Node(self.n, self.state, self.parent))
+            left_node.state[x][y], left_node.state[x][y-1] = left_node.state[x][y-1], left_node.state[x][y]
+            return left_node
+        return None
 
-def pos_blank_tile(state):
-    for i in range(len(state)):
-        for j in range(len(state)):
-            if state[i][j] == 0:
-                return (i,j)
+    def right(self, pos):
+        x,y = pos[0], pos[1]
+        if y != (self.n - 1):
+            right_node = Node(self.n, copy.deepcopy(self.state), Node(self.n, self.state, self.parent))
+            right_node.state[x][y], right_node.state[x][y+1] = right_node.state[x][y+1], right_node.state[x][y]
+            return right_node
+        return None
+
+    def pos_blank_tile(self):
+        for i in range(len(self.state)):
+            for j in range(len(self.state)):
+                if self.state[i][j] == 0:
+                    return (i,j)
+
+
+    def generate_children(self, pos):
+        
+        children = []
+        up_move = self.up(pos)
+        down_move = self.down(pos)
+        left_move = self.left(pos)
+        right_move = self.right(pos)
+        
+        if up_move:
+            children.append(up_move) 
+        if down_move:
+            children.append(down_move)
+        if left_move:
+            children.append(left_move)
+        if right_move:
+            children.ap pend(right_move)
+        
+        return children
 
 
 if __name__== "__main__":
@@ -70,26 +80,30 @@ if __name__== "__main__":
     input_state = [[1, 6, 2, 3], [9,5, 7, 4], [0, 10, 11, 8] , [13, 14, 15, 12]]
     goal_state = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]] 
 
-
+    input_node = Node(4, input_state, None)
     queue = deque()
-    queue.append(input_state)
+    queue.append(input_node)
 
     visited_states = []
-
     t = time.time()
 
     while(1):
 
-        current_state = queue.popleft()
-        print(current_state, end = '\n')
-        if current_state == goal_state:
-            print("Goal Found")
+        current_node = queue.popleft()
+        print(current_node, end = '\n')
+        if current_node.state == goal_state:
+            print("Goal Found\n")
+            print("Shortest path:\n")
+            print(current_node)
+            while(current_node.state != input_state):
+                current_node = current_node.parent
+                print(current_node)
             break
 
-        if current_state not in visited_states:
-            visited_states.append(current_state)
-            pos = pos_blank_tile(current_state)
-            children = generate_children(pos, current_state) 
+        if current_node.state not in visited_states:
+            visited_states.append(current_node.state)
+            pos = current_node.pos_blank_tile()
+            children = current_node.generate_children(pos) 
 
             for child in children:
                 queue.append(child)
